9b4d21f0d91102ebec7dcddd0ec44b31d72be6fd
agrochal <artur@procer.pl>
2020-05-17 23:03:07 +0200

    Code quality improvement

--- app.js
+++ app.js
@@ -7,58 +7,55 @@ const config = require('./config');
 const fetch = require('node-fetch');
 
 let jsondata;
-var T = new Twit(config.keys);
-
+let T = new Twit(config.keys);
 
 function apod() {
-   fetch("https://api.nasa.gov/planetary/apod?api_key=" + config.api)
-      .then(function(u) {
-         return u.json();
-      }).then(
-         function(json) {
-            jsondata = json;
-            download(jsondata.url, 'photo.jpg')
-         }
-      )
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${config.api}`)
+    .then(function(u) {
+      return u.json();
+    }).then(
+      function(json) {
+        jsondata = json;
+        download(jsondata.url, 'photo.jpg')
+      }
+    )
 }
 
 function download(url, filename) {
-   request.head(url, downloading);
+  request.head(url, downloading);
 
-   function downloading(err, res, body) {
-      request(url).pipe(fs.createWriteStream(filename)).on('close', finished);
-   }
+  function downloading(err, res, body) {
+    request(url).pipe(fs.createWriteStream(filename)).on('close', finished);
+  }
 
-   function finished() {
-      var b64content = fs.readFileSync('photo.jpg', {
-         encoding: 'base64'
-      });
-      T.post('media/upload', {
-         media_data: b64content
-      }, uploaded);
-   }
+  function finished() {
+    let b64content = fs.readFileSync('photo.jpg', {
+      encoding: 'base64'
+    });
+    T.post('media/upload', {
+      media_data: b64content
+    }, uploaded);
+  }
 }
 
 
 
 function uploaded(err, data, response) {
-
-   var mediaIdStr = data.media_id_string;
-   var params = {
-      status: jsondata.title + ' by ' + jsondata.copyright + '\n' + jsondata.date,
-      media_ids: [mediaIdStr]
-   }
-   T.post('statuses/update', params, tweeted);
+  const mediaIdStr = data.media_id_string;
+  const params = {
+    status: `${jsondata.title} by ${jsondata.copyright}\n${jsondata.date}`,
+    media_ids: [mediaIdStr]
+  }
+  T.post('statuses/update', params, tweeted);
 };
 
 function tweeted(err, data, response) {
-   if (err) {
-      console.log(err);
-   } else {
-      console.log('Success: ' + data.text);
-   }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Success: ${data.text}`);
+  }
 };
-
 
 apod();
 setInterval(apod, 1000 * 60 * 60 * 24);
